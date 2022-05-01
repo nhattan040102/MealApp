6c14241a3c7587d2a43c671f1f12fa99a48ea20e
nhattan040102-acc <tantran.040102@gmail.com>
2022-05-01 23:07:25 +0700

    add font and load font

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import *  as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
+
+const fetchFont = () => {
+  Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+    'open-sans-light': require('./assets/fonts/OpenSans-Light.ttf'),
+  })
+};
 
 export default function App() {
+  const [dataLoaded, setDataLoaded] = useState(false);
+  if (!dataLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFont}
+        onFinish={() => { setDataLoaded(true) }}
+        onError={error => console.log(error)}
+      />
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Text style={{ fontFamily: 'open-sans-bold' }}>Hello world</Text>
+
+    </View >
   );
 }
 
